51d326290266655007bd58c0b9c75f270f30f30d
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2025-11-28 14:23:54 -0500

    Add Support for Mbed TLS 2.28.10 (#7)
    Allow Mbed TLS 2.28.10 to be fuzzed.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 readonly ARCHIVE_SUFFIX='.tar.gz'
+readonly MBEDTLS_2_28='mbedtls-2.28.10'
+readonly SHA_256_2_28='c785ddf2ad66976ab429c36dffd4a021491e40f04fe493cfc39d6ed9153bc246'
+readonly MBEDTLS_2_3='mbedtls-2.3.0'
+readonly SHA_256_2_3='1614ee70be99a18ca8298148308fb725aad4ad31c569438bb51655a4999b14f9'
 readonly MBEDTLS_2_3='mbedtls-2.3.0'
 readonly SHA_256_2_3='1614ee70be99a18ca8298148308fb725aad4ad31c569438bb51655a4999b14f9'
 readonly MBEDTLS_2_1='mbedtls-2.1.5'
 readonly SHA_256_2_1='01e1325896cbeea55ac50b94e3818218a5bfff065e5b6e7a2a12987f5e187026'
 readonly MBEDTLS_1_3='mbedtls-1.3.17'
 readonly SHA_256_1_3='5a80deaa77f098733861b53cbabd392cee425b54109c90a6a2142a9662691aa7'
-readonly MBEDTLS_A=( "$MBEDTLS_2_3" "$MBEDTLS_2_1" "$MBEDTLS_1_3" )
-readonly SHA_256_A=( "$SHA_256_2_3" "$SHA_256_2_1" "$SHA_256_1_3" )
+readonly MBEDTLS_A=( "$MBEDTLS_2_28" "$MBEDTLS_2_3" "$MBEDTLS_2_1" "$MBEDTLS_1_3" )
+readonly SHA_256_A=( "$SHA_256_2_28" "$SHA_256_2_3" "$SHA_256_2_1" "$SHA_256_1_3" )
 readonly FOLDER_PREFIX='mbedtls-'
 readonly NO_TIME=1
 
