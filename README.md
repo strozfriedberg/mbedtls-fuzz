0adf2dc5bc940a3837b3f0fd5487ee9b6f04b791
faf0 <3429782+faf0@users.noreply.github.com>
2025-11-28 14:07:52 -0500

    Support AFL++ 4.34c
    Rewrite the scripts to support AFL++ 4.34c, which is the latest release
    at the time of writing.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About
 
-This project provides code and tools which allow for fuzzing the [mbed TLS library](https://tls.mbed.org/) using the [american fuzzy lop (afl) fuzzer](https://lcamtuf.coredump.cx/afl/).
+This project provides code and tools which allow for fuzzing the [mbed TLS library](https://tls.mbed.org/) using the [afl++ fuzzer](https://github.com/AFLplusplus/AFLplusplus).
 An integral part of this project is the code for a self-communicating instance (the client and server run in a single process) of mbed TLS.
 
 More information about the project is available on the Gotham Digital Science blog: 
@@ -11,32 +11,39 @@ More information about the project is available on the Gotham Digital Science bl
 
 ## Installation
 
-Grab the latest version of afl from the [afl homepage](https://lcamtuf.coredump.cx/afl/) and compile it.
-At the time of writing, the latest version is [afl 2.57b](https://github.com/google/AFL/archive/refs/tags/v2.57b.tar.gz).
+Grab the latest version of afl++ from the [afl++ homepage](https://github.com/AFLplusplus/AFLplusplus).
+[Build it](https://github.com/AFLplusplus/AFLplusplus/blob/stable/docs/INSTALL.md) instead of running it in Docker to maximize performance.
+At the time of writing, the latest version is [afl++ v4.34c](https://github.com/AFLplusplus/AFLplusplus/releases/tag/v4.34c).
 
-Set the `AFL_PATH` environment variable with the folder containing the afl binaries: `export AFL_PATH=/usr/local/src/afl-2.57b`
+Set the `AFL_PATH` environment variable with the folder containing the afl binaries: `export AFL_PATH=/usr/local/src/AFLplusplus`
 Extend the `PATH` environment variable with `AFL_PATH`: `export PATH=$PATH:$AFL_PATH`
 Alternatively, update the constants of the scripts in the `fuzz` folder so they point to the desired afl compiler.
 
 Run the following command which automatically downloads different versions of mbed TLS, patches them, compiles the code, and sets everything up for fuzzing.
 
-~~~
+```shell
 ./setup.sh
-~~~
+```
 
 ## Fuzzing
 
 Change to `fuzz` subdirectory inside the mbed TLS directory that you wish to fuzz.
-Run the `fuzz.sh` script.
-Running the script without arguments prints the usage screen including a description of the tool.
-The script requires the network packet number (at least `1`) that should be fuzzed and the fuzzer number (use `1` to launch the master instance; higher numbers launch slaves).
+Run the `./fuzz.sh` script.
 
-In the following example, we launch a master instance to fuzz network packet 3 of the self-communicating mbed TLS 2.3.0 binary:
+Running the script without arguments creates the original client and server network packets to be fuzzed and prints the usage screen with a description of the tool.
 
-~~~
-cd mbedtls-2.3.0/fuzz
+To fuzz a specific network packet, execute the script with the packet number (minimum of `1`) followed by the fuzzer instance number as command-line arguments:
+* Use 1 to initiate the primary fuzzer instance.
+* Unique numbers greater than `1` will start additional fuzzer instances.
+
+In the following example, we launch a main fuzzer instance to fuzz network packet 3 of the self-communicating mbed TLS 2.3.0 binary:
+
+```shell
+cd mbedtls-mbedtls-2.3.0/fuzz/
 ./fuzz.sh 3 1
-~~~
+```
+
+The `fuzz.sh` script mounts a tmpfs at `/tmp/afl-ramdisk/mbedtls` containing the fuzzing input and output files for performance reasons.
 
 ## Crash Analysis
 
@@ -45,9 +52,9 @@ Finally, run `./crash-analysis.sh` which allows you to debug the crash using `gd
 
 You can use a command such as the following command to create the file containing the paths of the crash files:
 
-~~~
+```shell
 find . -name 'id*' -type f | grep crashes | sort > crash_files.txt
-~~~
+```
 
 ## Copyright
 
--- fuzz/fuzz.sh
+++ fuzz/fuzz.sh
@@ -4,18 +4,6 @@ readonly SELFTLS_BIN='../selftls'
 # Add afl-fuzz to your PATH or the change the following variable:
 readonly AFL_FUZZ='afl-fuzz'
 readonly RAMDISK_PATH='/tmp/afl-ramdisk/mbedtls'
-# The following parameter is the peak virtual memory use in MiB of selftls.
-# The parameter is only necessary for ASAN-enabled builds on x86-64.
-# You can determine the value for your selftls using the tool from
-# http://jwilk.net/software/recidivm
-# by running 'recidivm -u M selftls'.
-readonly MEM_REQUIRED='300000000'
-# If you want to use the experimental cgroups script under Linux for
-# ASAN-enabled builds on x86-64, set the following path correctly and
-# uncomment the relevant code further down:
-readonly AFL_ASAN_CGROUPS="${AFL_PATH}/experimental/asan_cgroups/limit_memory.sh"
-# Set to 0, if you do not have an ASAN-enabled build.
-export AFL_USE_ASAN=1
 
 usage() {
 	local progname=$1
@@ -28,10 +16,11 @@ usage() {
 	Alternatively, run './selftls' manually to write the packets to files.
 	Running './selftls' allows you to check if there are any errors running the program that we fuzz.
 	A specific network packet can be replaced with the content from a file which allows for fuzzing that packet.
-	To fuzz a specific packet, provide the packet number (1-based; check names of the 'packet-*' files to get the possible packet numbers) and the fuzzer number as command-line arguments.
-	The master fuzzer has number 1, while slaves can have any other number.
+	To fuzz a specific packet, provide the following two command-line arguments:
+	* 1-based packet number, referencing a 'packet-*' file
+	* 1 for the main fuzzer or any unique, higher number for each additional fuzzer
 	
-	mbedtls-fuzz v3.0
+	mbedtls-fuzz v3.1
 	Fabian Foerg <ffoerg@gdssecurity.com>
 	https://blog.gdssecurity.com/labs/2015/9/21/fuzzing-the-mbed-tls-library.html
 	Copyright 2015-2025 Gotham Digital Science
@@ -53,27 +42,23 @@ main() {
 		sudo mount -t tmpfs -o size=512M tmpfs "$RAMDISK_PATH"
 	fi
 	cp -R . "$RAMDISK_PATH"
-	cd "$RAMDISK_PATH"
+	pushd "$RAMDISK_PATH"
 
-	local subfolder=""
+	local subfolder=''
 	if [ "1" = "$2" ]; then
 		# Master creates subfolder
 		subfolder="$(date --rfc-3339=seconds)"
 		mkdir "$subfolder"
 	else
 		# Slaves use the folder with newest date in the name
-		subfolder=$(find . -maxdepth 1 -type d -regextype posix-egrep -iregex ".*[0-9]{4}-[0-9]{2}-[0-9]{2} .*" -print | sort | tail -1)
+		subfolder="$(find . -maxdepth 1 -type d -regextype posix-egrep -iregex '.*[0-9]{4}-[0-9]{2}-[0-9]{2} .*' -print | sort | tail -1)"
 	fi
-	cd "$subfolder"
+	pushd "$subfolder"
 
 	local packet_no="$1"
 	local FUZZER_NAME="packet-${packet_no}--fuzzer-$2"
 	if [ "1" = "$2" ]; then
 		# Master mode
-
-		# Configure system for fuzzing
-		echo core | sudo tee /proc/sys/kernel/core_pattern
-		sudo sh -c "cd /sys/devices/system/cpu && echo performance | tee cpu*/cpufreq/scaling_governor > /dev/null"
 
 		# Create directories
 		mkdir -p fin sync
@@ -83,21 +68,10 @@ main() {
 		"$SELFTLS_BIN"
 		cp "packet-$packet_no" fin
 
-		if [ "1" = "$AFL_USE_ASAN" ]; then
-			"$AFL_FUZZ" -i fin -o sync -m "$MEM_REQUIRED" -M "$FUZZER_NAME" "$SELFTLS_BIN" "$packet_no" @@
-			# If you want to use the experimental cgroups script under Linux:
-			#sudo swapoff -a; sudo "$AFL_ASAN_CGROUPS" -u "$USER" "$AFL_FUZZ" -i fin -o sync -m none "$SELFTLS_BIN" "$packet_no" @@
-		else
-			"$AFL_FUZZ" -i fin -o sync -M "$FUZZER_NAME" "$SELFTLS_BIN" "$packet_no" @@
-		fi
-
+		"$AFL_FUZZ" -i fin -o sync -M "$FUZZER_NAME" "$SELFTLS_BIN" "$packet_no" @@
 	else
 		# Slave mode
-		if [ "1" = "$AFL_USE_ASAN" ]; then
-			"$AFL_FUZZ" -i fin -o sync -m "$MEM_REQUIRED" -S "$FUZZER_NAME" "$SELFTLS_BIN" "$packet_no" @@
-		else
-			"$AFL_FUZZ" -i fin -o sync -S "$FUZZER_NAME" "$SELFTLS_BIN" "$packet_no" @@
-		fi
+		"$AFL_FUZZ" -i fin -o sync -S "$FUZZER_NAME" "$SELFTLS_BIN" "$packet_no" @@
 	fi
 }
 
